dbe3fc4fd01d5507bc460914a619937acf3d2cfd
deluxscript <delsam94@yahoo.com>
2018-03-14 01:16:11 +0100

    Auto-committing 'AddTask.js'

--- src/components/Dashboard/AddTask.js
+++ src/components/Dashboard/AddTask.js
@@ -5,23 +5,23 @@ const AddTask = () => {
 	return (
 		<div className="card default-bg AddTask">
 			<div className="card-body">
-				<h4 class="header">Add your task details here</h4>
-				<form class="task-form">
-					<div class="form-group">
-					         <input type="text" class="form-control" placeholder="Project Name" />
+				<h4 className="header">Add your task details here</h4>
+				<form className="task-form">
+					<div className="form-group">
+					         <input type="text" className="form-control" placeholder="Project Name" />
 					</div>
 					<div className="form-group">
-						<textarea class="form-control border-input" placeholder="This is a textarea with border. Here can be your nice text" rows="3"></textarea>
+						<textarea className="form-control border-input" placeholder="This is a textarea with border. Here can be your nice text" rows="3"></textarea>
 					</div>
-					<div class="form-group">
-						<div class='input-group date' id='datetimepicker'>
-							<input type='text' class="form-control datetimepicker" placeholder="Start Date" />
-							<span class="input-group-addon">
-							<span class="glyphicon glyphicon-calendar"><i class="fa fa-calendar" aria-hidden="true"></i></span>
+					<div className="form-group">
+						<div className='input-group date' id='datetimepicker'>
+							<input type='text' className="form-control datetimepicker" placeholder="Start Date" />
+							<span className="input-group-addon">
+							<span className="glyphicon glyphicon-calendar"><i className="fa fa-calendar" aria-hidden="true"></i></span>
 							</span>
 						</div>
 					</div>
-					<button type="button" class="btn btn-success btn-sm">Submit</button>
+					<button type="button" className="btn btn-success btn-sm">Submit</button>
 				</form>
 			</div>
 		</div>
