8b1ba6ee61960efa7cb5b4039a287089ef0b6194
deluxscript <delsam94@yahoo.com>
2018-03-14 01:41:25 +0100

    Auto-committing 'AddTask.js'

--- src/components/Dashboard/AddTask.js
+++ src/components/Dashboard/AddTask.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, {Component}from 'react';
 import './AddTask.css'
 
-const AddTask = () => {
-	return (
-		<div className="card default-bg AddTask">
-			<div className="card-body">
-				<h4 className="header">Add your task details here</h4>
-				<form className="task-form">
-					<div className="form-group">
-					         <input type="text" className="form-control" placeholder="Project Name" />
-					</div>
-					<div className="form-group">
-						<textarea className="form-control border-input" placeholder="This is a textarea with border. Here can be your nice text" rows="3"></textarea>
-					</div>
-					<div className="form-group">
-						<div className='input-group date' id='datetimepicker'>
-							<input type='text' className="form-control datetimepicker" placeholder="Start Date" />
-							<span className="input-group-addon">
-							<span className="glyphicon glyphicon-calendar"><i className="fa fa-calendar" aria-hidden="true"></i></span>
-							</span>
+class AddTask extends Component {
+
+	render() {
+		return (
+			<div className="card default-bg AddTask">
+				<div className="card-body">
+					<h4 className="header">Add your task details here</h4>
+					<form className="task-form">
+						<div className="form-group">
+						         <input type="text" className="form-control" placeholder="Project Name" />
 						</div>
-					</div>
-					<button type="button" className="btn btn-success btn-sm">Submit</button>
-				</form>
+						<div className="form-group">
+							<textarea className="form-control border-input" placeholder="This is a textarea with border. Here can be your nice text" rows="3"></textarea>
+						</div>
+						<div className="form-group">
+							<div className='input-group date' id='datetimepicker'>
+								<input type='text' className="form-control datetimepicker" placeholder="Start Date" />
+								<span className="input-group-addon">
+								<span className="glyphicon glyphicon-calendar"><i className="fa fa-calendar" aria-hidden="true"></i></span>
+								</span>
+							</div>
+						</div>
+						<button type="button" className="btn btn-success btn-sm">Submit</button>
+					</form>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default AddTask;
