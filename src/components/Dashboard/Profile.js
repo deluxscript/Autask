3d08460a34a4853411e973fbd7b91cfd26f92a86
deluxscript <delsam94@yahoo.com>
2018-03-14 01:16:37 +0100

    Auto-committing 'Profile.js'

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -3,20 +3,20 @@ import './Profile.css'
 
 const Profile = () => {
 	return (
-		<div class="card card-profile">
-			<div class="card-cover"></div>
-			<div class="card-avatar border-white">
+		<div className="card card-profile">
+			<div className="card-cover"></div>
+			<div className="card-avatar border-white">
 				<a>
 					<img src="" alt="profileimg" />
 				</a>
 			</div>
-			<div class="card-body">
-				<h4 class="card-title">Ayo Michael</h4>
-				<p class="card-description">
+			<div className="card-body">
+				<h4 className="card-title">Ayo Michael</h4>
+				<p className="card-description">
 				Hey there! Welcome to your user card. As you can see, it is already looking great.
 				</p>
-				<span class="label label-primary">dele@enterfive.com</span>
-				<button type="button" class="btn btn-danger">Log Out</button>
+				<span className="label label-primary">dele@enterfive.com</span>
+				<button type="button" className="btn btn-danger">Log Out</button>
 			</div>
 		</div>
 	);
