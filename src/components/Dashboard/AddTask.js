6e84e912ba6c3edad837ad160a0666916a764ef4
deluxscript <delsam94@yahoo.com>
2018-03-15 02:08:57 +0100

    Auto-committing 'AddTask.js'

--- src/components/Dashboard/AddTask.js
+++ src/components/Dashboard/AddTask.js
@@ -38,14 +38,14 @@ class AddTask extends Component {
 					<h4 className="header">Add your task details here</h4>
 					<form className="task-form">
 						<div className="form-group">
-						         <input type="text" name="projectName" className="form-control" placeholder="Project Name" onChange={ event => this.handleChange} />
+						         <input type="text" name="projectName" className="form-control" placeholder="Project Name" onChange={(e) => this.handleChange} />
 						</div>
 						<div className="form-group">
-							<textarea className="form-control border-input" name="ProjectDesc" placeholder="This is a textarea with border. Here can be your nice text" rows="3" onChange={ event => this.handleChange} ></textarea>
+							<textarea className="form-control border-input" name="ProjectDesc" placeholder="This is a textarea with border. Here can be your nice text" rows="3" onChange={(e) => this.handleChange} ></textarea>
 						</div>
 						<div className="form-group">
 							<div className='input-group date' id='datetimepicker'>
-								<input type='text' name="assignedDate" className="form-control datetimepicker" placeholder="Start Date" onChange={ event => this.handleChange} />
+								<input type='text' name="assignedDate" className="form-control datetimepicker" placeholder="Start Date" onChange={(e) => this.handleChange} />
 								<span className="input-group-addon">
 								<span className="glyphicon glyphicon-calendar"><i className="fa fa-calendar" aria-hidden="true"></i></span>
 								</span>
