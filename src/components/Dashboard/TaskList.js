71cf6175f2c7fa680a85afd52314c389dafbf6b1
deluxscript <delsam94@yahoo.com>
2018-03-20 03:43:10 +0100

    Auto-committing 'TaskList.js'

--- src/components/Dashboard/TaskList.js
+++ src/components/Dashboard/TaskList.js
@@ -11,7 +11,7 @@ class TaskList extends Component {
 				<div className="col-md-12 col-sm-12">
 					<div className="card">
 						<div className="card-body">
-							<h6 className="card-category">Best stories</h6>
+							<h6 className="card-category">{Task.projectName}</h6>
 							<span className="label label-primary">12/11/2018</span>
 							<div id="acordeon">
 								<div id="accordion" role="tablist" aria-multiselectable="true">
@@ -25,9 +25,7 @@ class TaskList extends Component {
 											</h5>
 										</div>
 										<div id="collapseOne" className="collapse" role="tabpanel" aria-labelledby="headingOne">
-											<div className="card-body">
-											Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-											</div>
+											<div className="card-body">											</div>
 										</div>
 									</div>
 								</div>
