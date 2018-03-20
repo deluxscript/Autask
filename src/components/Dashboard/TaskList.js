3cc00495443d115472d4a847cadf93f31c47e239
deluxscript <delsam94@yahoo.com>
2018-03-20 03:40:51 +0100

    Auto-committing 'TaskList.js'

--- src/components/Dashboard/TaskList.js
+++ src/components/Dashboard/TaskList.js
@@ -5,7 +5,7 @@ class TaskList extends Component {
 	render() {
 		const { eachTask, index } = this.props;
 		// const Task = eachTask;
-		console.log('TaskList', eachTask);
+		console.log('TaskList', eachTask.projectName);
 		return (
 			<div className= "row TaskList">
 				<div className="col-md-12 col-sm-12">
